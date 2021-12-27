845d5e21d2126893ace0115d7dbaad97d9a79314
Dplo1514 <dladlsgur3334@gmail.com>
2021-12-27 22:42:29 +0900

    211227 RfhHanghae

---
+++ src/main/java/overLoading.java
@@ -0,0 +1,13 @@
+public class overLoadingRiding {
+    public static void main(String[] args) {
+
+    }
+
+    int add(int x , int y , int z){
+        return x + y + z;
+    }
+
+    long add(int a , int b , int c){
+        return a + b + c;
+    }
+}
